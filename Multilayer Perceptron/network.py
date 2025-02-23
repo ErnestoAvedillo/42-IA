dfbbe8139d3a818f3ffd320da644be364de9fcf1
ErnestoAvedillo <eavedillo@yahoo.es>
2025-02-23 11:05:24 +0100

    remove not needed global variables in class

--- Multilayer Perceptron/network.py
+++ Multilayer Perceptron/network.py
@@ -1,53 +1,66 @@
 import numpy as np
-from activation import Activation
 from layer import Layer
 
 class Network:
     def __init__(self,layers = [], x = None, y = None, learning_rate = 0.001):
         self.layers = layers
-        self.x = None
-        self.y_pred = None
-        self.y = None
         self.learning_rate = None
 
     def add_layer(self, layer):
         self.layers.append(layer)
     
-    def add_layer(self, nodes, input_dim=None, activation="sigmoid"):
+    def add_layer(self, layer = None, nodes = None, input_dim=None, activation="sigmoid"):
+        if layer is not None:
+            self.layers.append(layer)
+            return
+        if nodes is None:
+            raise ValueError("nodes parameter must be defined")
         if input_dim is None and len(self.layers) == 0:
             raise ValueError("First layer must have input_dim")
         else:
-            input = input_dim if input_dim is not None else self.layers[-1].output_dim
+            input = input_dim if input_dim is not None else self.layers[-1].nodes
         self.layers.append(Layer(input, nodes, activation))
 
-    def forward(self):
+    def forward(self, X):
+        current_input = X
         for layer in self.layers:
-            self.y_pred = layer.forward(self.x)
-        return
+            current_input = layer.forward(current_input)
+        return current_input
     
-    def backward(self):
-        delta = self.y_pred - self.y
+    def backward(self,delta):
         for layer in reversed(self.layers):
-            delta=layer.backward(delta, self.learning_rate)
+            delta=layer.backward(delta, self.learning_rate).mean(axis=2)
 
-    def train(self, x, y, x_test, y_test, epochs=1000, accuracy=0.99, learning_rate=0.001):
-        self.x = x
-        self.y = y
+    def train(self, X, Y, x_test, y_test, epochs=1000, accuracy=0.99, learning_rate=0.001, batch_size=None):
+        factor_theta0 = np.concat((np.ones([1]),-(X.mean(axis = 0)/ X.std(axis = 0))), axis = 0)
+        X = (X - np.mean(X, axis = 0)) / (np.std(X, axis = 0))
+        y_pred = np.zeros(Y.shape)
         self.learning_rate = learning_rate
         for _ in range(epochs):
-            for i in range(len(x)):
-                output = self.forward(x[i])
-                delta = output - y[i]
+            if batch_size is None:
+                y_pred = self.forward(X)
+                delta = y_pred - Y
                 self.backward(delta)
-                print(f"Epoch {_} - Train Accuracy: {np.mean(output.argmax(axis=1) == y[i])}", end="\r")
-            print(f"Epoch {_} - Train Accuracy: {np.mean(output.argmax(axis=1) == y[i])}", end="\t")
-            test_pred = self.predict(x_test).argmax(axis=1)
-            print(" - Test Accuracy: {np.mean(test_pred == y_test)}")
+                print(f"Epoch {_} - Train Accuracy: {np.mean(y_pred.argmax() == Y)}", end="\t")
+            else:
+                batches_x = np.array_split(x, np.ceil(x.shape[0] / batch_size))
+                batches_y = np.array_split(y, np.ceil(y.shape[0] / batch_size))
+                i = 0
+                for i in range(batches_x):
+                    x = batches_x[i]
+                    y = self.forward(x)
+                    delta = batches_y[i] - y
+                    self.backward(delta)
+                    print(f"Epoch {_} -batch {i} - Train Accuracy: {np.mean(y.argmax() == y)}", end="\r")
+                y_pred = self.forward(X)
+                print(f"Epoch {_} - Train Accuracy: {np.mean(y_pred.argmax(axis=1) == X)}", end="\t")
+            test_pred = self.predict(x_test).argmax()
+            print(f" - Test Accuracy: {np.mean(test_pred == y_test)}")
             if np.mean(test_pred == y_test) > accuracy:
                 break
 
     def predict(self, x):
-        return np.array([self.forward(x_) for x_ in x])
+        return self.forward(x)
 
     def __str__(self):
         return "\n".join([str(layer) for layer in self.layers])

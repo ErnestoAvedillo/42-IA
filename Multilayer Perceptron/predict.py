4f97cae3408baae3b3bbd6d155043ed342c19b1b
ErnestoAvedillo <eavedillo@yahoo.es>
2025-02-25 23:05:52 +0100

    clean data done in other script

--- Multilayer Perceptron/predict.py
+++ Multilayer Perceptron/predict.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import numpy as np
-from histogram import show_all_histograms
 from sklearn.model_selection import train_test_split
-
 from layer import Layer
 from network import Network
 import sys
 
 def load_data(file_path):
-    data = pd.read_csv(file_path, index_col=None, header=None)
+    data = pd.read_csv(file_path, index_col=None, header=0)
     data = data.dropna()
     data = data.drop_duplicates()
     data = pd.get_dummies(data)
@@ -23,27 +21,28 @@ try:
 except FileNotFoundError:
     print("File not found")
     sys.exit(1)
-print("imprimo los datos")
-print(data)
-print("imprimo la descripcion de los datos")
-print(data.describe())
-print("imprimo las keys  de los datos")
-keys = data.keys()
-print (keys)
-#show_all_histograms(data)
-for key in keys:
-    if data[key].value_counts().count() > 10:
-        continue
-    print(key, data[key].unique())
-    print(key, data[key].value_counts())
-Y = data[["1_B","1_M"]].to_numpy().astype(int)
-X = data.drop(columns=[0,"1_B","1_M"]).to_numpy().astype(float)
-print(X)
-print(Y)
+
+print (data.keys())
+#input("Press enter..")
+
+Y = data["1_B"].to_numpy().astype(int)
+#Y = data[["1_B","1_M"]].to_numpy().astype(int)
+X = data.drop(columns=["1_B"]).to_numpy().astype(float)
+#X = (X - np.mean(X, axis = 0)) / (np.std(X, axis = 0))
+#print(X)
+#print(Y)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-network = Network()
-network.add_layer(layer = Layer(nodes = 30, input_dim=30))
-network.add_layer(nodes = 20)
-network.add_layer(nodes = 10)
-network.add_layer(nodes = 2)
-network.train(X_train, Y_train, X_test, Y_test, epochs=100)
+
+network = {}
+accuracies = {}
+i = 0
+for j in range(5,21):
+    network[i] = Network()
+    network[i].add_layer(layer = Layer(nodes = 30, input_dim=30))
+    network[i].add_layer(nodes = j)
+    #network.add_layer(nodes = 2, activation="softmax")
+    network[i].add_layer(nodes = 1)
+    acc = network[i].train(X_train, Y_train, X_test, Y_test, epochs=100)
+    accuracies[i] = acc
+    i += 1
+print(accuracies)

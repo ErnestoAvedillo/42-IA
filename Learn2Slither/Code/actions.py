f7a119cf6a13c87d9859a4f166f197bc84f091fb
ErnestoAvedillo <eavedillo@yahoo.es>
2025-06-01 17:56:43 +0200

    add method to get action name

--- Learn2Slither/Code/actions.py
+++ Learn2Slither/Code/actions.py
@@ -1,6 +1,12 @@
 from enum import Enum
 from random import choice
 
+NAMES = {
+    0: "UP",
+    1: "DOWN",
+    2: "LEFT",
+    3: "RIGHT"
+}
 
 class Action(Enum):
     # Actions for the snake
@@ -16,3 +22,10 @@ class Action(Enum):
     def get_action(action: int):
         """Get the action corresponding to the action int."""
         return Action(action) if action in range(len(Action)) else None
+
+    def get_len_actions():
+        return len(Action)
+
+    def get_action_name(self):
+        """Print the name of the action."""
+        return (NAMES[self.value])

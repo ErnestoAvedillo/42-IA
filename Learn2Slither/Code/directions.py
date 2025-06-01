0eb76f5b20c44065a712e89e6625635f27689701
ErnestoAvedillo <eavedillo@yahoo.es>
2025-06-01 17:57:50 +0200

    directions can be selected with different variable types

--- Learn2Slither/Code/directions.py
+++ Learn2Slither/Code/directions.py
@@ -10,9 +10,26 @@ class Directions(Enum):
     LEFT = [-1, 0]
     RIGHT = [1, 0]
 
-    def get_direction(action: Action):
-        """Get the direction corresponding to the action."""
-        return Directions[action.value] if action in Action else None
+    def get_direction(action):
+        """Get the direction corresponding to the action where
+        each action corresponds to this mapping in case is an integer:
+        0: UP
+        1: DOWN
+        2: LEFT
+        3: RIGHT
+        """
+        if isinstance(action, int):
+            directions = list(Directions)
+            if 0 <= action < len(directions):
+                return directions[action].value
+        elif isinstance(action, Action):
+            action = Action.get_action_index(action)
+            return Directions.get_direction(action)
+        elif isinstance(action, Directions):
+            # If action is already a Directions enum, return its value
+            return action.value
+        else:
+            raise ValueError(f"Invalid action index: {action}")
 
     def get_random_direction():
         """Return a random direction from the Directions enum."""

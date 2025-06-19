c69f48620869074339e0ee7120d64edb9c69bc8b
Ernesto Avedillo <eavedillo@yahoo.es>
2025-06-19 18:41:17 +0200

    optimize parameters

--- Learn2Slither/Code/learn_snake.py
+++ Learn2Slither/Code/learn_snake.py
@@ -27,29 +27,27 @@ def Usage():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a DQN agent for the Snake game.')
-    parser.add_argument('-l', '--learn_type', type=str, choices=['Q_LEARNING', 'SARSA'], help='Type of learning algorithm to use.')
-    parser.add_argument('-f', '--file_name', type=str, nargs='?', default='dqn_snake_model.joblib', help='File name to save the model.')
-    parser.add_argument('-g', '--gpu_nr', type=int, nargs='?', default=0, help='GPU number where to xecute the neural network.')
-    parser.add_argument('-h', '--history_lengths', type=str, nargs='?', default='lengths.csv', help='File name to save the hystoric of lengths for each game played.')
+    parser.add_argument('-l', '--learn', type=str, choices=['Q_LEARNING', 'SARSA'], help='Type of learning algorithm to use.')
+    parser.add_argument('-f', '--file_model', type=str, nargs='?', default='dqn_snake_model.joblib', help='File name to save the model.')
+    parser.add_argument('-g', '--gpu_nr', type=int, nargs='?', help='GPU number where to xecute the neural network.')
+    parser.add_argument('--history_lengths', type=str, nargs='?', default='lengths.csv', help='File name to save the hystoric of lengths for each game played.')
+    parser.add_argument('-e', '--episodes', type=int, nargs='?', help='Number of maximum episodes to repeat.')
     args = parser.parse_args()
     
     # Check if learning type is provided, otherwise default to 'SARSA
-    if not args.learn_type:
+    if not args.learn:
         print("No learning type provided. Defaulting to 'Q_LEARNING'.")
         Learn_Type = "Q_LEARNING"
-    elif args.learn_type not in ["Q_LEARNING", "SARSA"]:
-        Usage()
-        raise ValueError("Invalid learning type. Choose 'Q_LEARNING' or 'SARSA'.")
     else:
-        print(f"Learning type set to: {args.learn_type}")
-        Learn_Type = args.learn_type
+        print(f"Learning type set to: {args.learn}")
+        Learn_Type = args.learn
     # Check if file name is provided, otherwise default to 'dqn_snake_model.joblib'
-    if not args.file_name:
+    if not args.file_model:
         print("No file name provided. Defaulting to 'dqn_snake_model.joblib'.")
         File_Name = "dqn_snake_model.joblib"
     else:
-        File_Name = args.file_name
-    if not args.gpu_nr:
+        File_Name = args.file_model
+    if not args.gpu_nr :
         print("No GPU number assigned. Default number taken 0")
         gpu_number = 0
     else:
@@ -59,6 +57,11 @@ if __name__ == "__main__":
         filename_lengths = "lengths.csv"
     else:  
         filename_lengths = args.history_lengths
+    if not args.episodes:
+        print("")
+        max_episodes = NUM_EPISODES
+    else:  
+        max_episodes = args.episodes
 
 env = EnvSnake(Nr_cells=[10, 10])
 agent = DQNAgent(state_shape=len(env.observation_space),
@@ -68,7 +71,7 @@ rewards = [0 for _ in range(Reward.get_len() + 1)] # initialize counter of rewar
 max_length = 3 
 first_time = time.time()
 lengths = []
-for i in range(NUM_EPISODES):
+for i in range(max_episodes):
     observation, info = env.reset()
     episode_over = False
     while not episode_over:
@@ -104,19 +107,19 @@ for i in range(NUM_EPISODES):
         #env.print_map_in_shell(clear=False)
         episode_over = terminated or truncated
         max_length = max (env.get_length_worn(), max_length)
-        print(f"Episode {i + 1}/{NUM_EPISODES} \t - Length {env.get_length_worn()} \t - Max_length {max_length} \t - Action: {Action(action).get_action_name()}")
+        print(f"Episode {i + 1}/{max_episodes} \t - Length {env.get_length_worn()} \t - Max_length {max_length} \t - Action: {Action(action).get_action_name()}")
         print(f"Reward: {reward} \t- Epsilon: {agent.epsilon:.4f} \t - Terminated: {terminated} \t- Truncated: {truncated}\t - Moves: {info['moves']}")
         print(f"Rewards historic:\t- NONE {rewards[0]}\t- RED {rewards[1]}\t- GREEN {rewards[2]}\t - IS_THE_WAY {rewards[5]}\t - IS_ALLIGNED {rewards[6]}")
         print(f"\t\t\t- WALL {rewards[3]}\t- BODY {rewards[4]}\t- UNHANDLED {rewards[8]}\t - REPEATED_POSITION {rewards[7]}")
         num_Chars = 50
-        percent = 100 * (i + 1) / NUM_EPISODES
+        percent = 100 * (i + 1) / max_episodes
         filled = int(num_Chars * percent // 100)
         bar = '█' * filled + '-' * (num_Chars - filled)
         print(f'', end='\r')
-        print(f"Time elapsed: {(time.time() - first_time) / 3600 :.2f} hours,\t|{bar}| {percent:.2f}% \t - time left: {(NUM_EPISODES - i - 1) * (time.time() - first_time) / (i + 1) / 3600:.2f} hours")
+        print(f"Time elapsed: {(time.time() - first_time) / 3600 :.2f} hours,\t|{bar}| {percent:.2f}% \t - time left: {(max_episodes - i - 1) * (time.time() - first_time) / (i + 1) / 3600:.2f} hours")
         # time.sleep(1)
     lengths.append(env.get_length_worn())
-    pd.DataFrame(lengths, columns=["length"]).to_csv("lengths.csv", index=False)
+    pd.DataFrame(lengths, columns=["length"]).to_csv(filename_lengths, index=False)
 episode_over = False
 lengths = np.array(lengths)
 plt.plot(lengths, label='Length of Snake')

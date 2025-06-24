34f310e7ae25b05c34ba1edd860316880a24b67c
ErnestoAvedillo <eavedillo@yahoo.es>
2025-06-24 22:54:50 +0200

    count rewards in a separate class

--- Learn2Slither/Code/test_snake.py
+++ Learn2Slither/Code/test_snake.py
@@ -3,7 +3,7 @@ from dqn_agent_keras import DQNAgent
 from actions import Action
 import time
 import sys
-
+from count_rewards import CountRewards
 
 if len(sys.argv) > 1:
     try:
@@ -23,24 +23,29 @@ print(f"Model loaded from {File_Name}")
 rewards = [0, 0, 0, 0, 0, 0]
 
 observation, info = env.reset()
+total_rewards = CountRewards()
 while not episode_over:
     action, is_aleatory = agent.choose_action(observation)
     observation, reward, terminated, truncated, info = env.step(action)
     env.print_map_in_shell()
     episode_over = terminated or truncated
-    match reward:
-        case 0:  # Reward.NONE.value
-            rewards[0] += 1
-        case 1:  # Reward.RED_APPLE.value
-            rewards[1] += 1
-        case 2:  # Reward.GREEN_APPLE.value
-            rewards[2] += 1
-        case 3:  # Reward.WALL_PENALTY.value
-            rewards[3] += 1
-        case 4:  # Reward.BODY_PENALTY.value
-            rewards[4] += 1
-        case _:
-            rewards[5] += 1  # Unhandled reward case
-    print(f"Action: {Action(action).get_action_name()}, Aleatory {is_aleatory}, Reward: {reward}, Episode Over: {episode_over}")
-    print(f"Rewards historic: NONE {rewards[0]}, RED {rewards[1]}, GREEN {rewards[2]}, WALL {rewards[3]}, BODY {rewards[4]}, UNHANDLED {rewards[5]}")
+    total_rewards.add_reward(reward)
+#    match reward:
+#        case 0:  # Reward.NONE.value
+#            rewards[0] += 1
+#        case 1:  # Reward.RED_APPLE.value
+#            rewards[1] += 1
+#        case 2:  # Reward.GREEN_APPLE.value
+#            rewards[2] += 1
+#        case 3:  # Reward.BODY_WALL_PENALTY.value
+#            rewards[3] += 1
+#        case 4:  # Reward.BODY_PENALTY.value
+#            rewards[4] += 1
+#        case _:
+#            rewards[5] += 1  # Unhandled reward case
+    print(f"Action: {Action(action).get_action_name()}, Aleatory {is_aleatory}, Reward: {reward.value}, Episode Over: {episode_over}")
+    for my_reward, name, value in total_rewards.total_rewards():
+        print(f"{name}, {value} --", end='\t')
+    print("")
+    #print(f"Rewards historic: NONE {rewards[0]}, RED {rewards[1]}, GREEN {rewards[2]}, WALL {rewards[3]}, BODY {rewards[4]}, UNHANDLED {rewards[5]}")
     time.sleep(1)

7ebcffe6667df06db758595fd3b284dcf2d97fb0
ErnestoAvedillo <eavedillo@yahoo.es>
2025-06-28 11:33:49 +0200

    include learning in GUI

--- Learn2Slither/Code/snake.py
+++ Learn2Slither/Code/snake.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from motor_snake import MotorSnake
+from env_snake import EnvSnake
 import numpy as np
 from dqn_agent_keras import DQNAgent
 from actions import Action
@@ -8,6 +9,9 @@ from rewards import Reward
 import time
 import os
 import argparse
+import platform
+import pandas as pd
+from matplotlib import pyplot as plt
 
 MAX_MOVES = 1000  # Maximum number of moves before truncation
 
@@ -33,10 +37,11 @@ RED = 2
 
 ACTIONS = ["NONE", "UP", "DOWN", "LEFT", "RIGHT"]
 
-class Snake(pg.sprite.Sprite, MotorSnake):
+
+class Snake(pg.sprite.Sprite, EnvSnake):
     def __init__(self, x, y, Nr_cells=[10, 10], modelname=None):
         pg.sprite.Sprite.__init__(self)
-        MotorSnake.__init__(self, Nr_cells)
+        EnvSnake.__init__(self, Nr_cells)
         self.size_cells = [x // self.nr_cells[0], y // self.nr_cells[1]]
         pg.display.set_caption("Snake Game")
         pg.font.init()
@@ -225,6 +230,9 @@ class Snake(pg.sprite.Sprite, MotorSnake):
                     elif event.key == pg.K_2:
                         self._auto_play()
                         return
+                    elif event.key == pg.K_3:
+                        self._learn_game()
+                        return
                     elif event.key == pg.K_ESCAPE:
                         pg.quit()
                         exit()
@@ -234,6 +242,9 @@ class Snake(pg.sprite.Sprite, MotorSnake):
                         return
                     elif self.auto_button.collidepoint(pg.mouse.get_pos()):
                         self._auto_play()
+                        return
+                    elif self.learn_button.collidepoint(pg.mouse.get_pos()):
+                        self._learn_game()
                         return
                 pg.event.clear()
             if self._autoplaying:
@@ -265,7 +276,7 @@ class Snake(pg.sprite.Sprite, MotorSnake):
         font = pg.font.SysFont('Arial', 48)
         text_surface = font.render("Game Over", True, (255, 255, 255))
         self.screen.blit(text_surface,
-                         ((self.nr_cells[0] // 2 - 4) * self.size_cells[0],
+                         ((self.nr_cells[0] // 2 - 3) * self.size_cells[0],
                           (self.nr_cells[1] // 2 - 1) * self.size_cells[1]))
         pg.display.flip()  # Update the full display
         self.clock.tick(1)
@@ -329,72 +340,73 @@ class Snake(pg.sprite.Sprite, MotorSnake):
         return (self.get_observation(), self.reward,
                 self.terminated, self.truncated, {})
 
-    def get_observation(self):
-        """
-        Returns the current observation of the environment.
-        The observation consists of the environment seen by the snake,
-        Structure:
-        head_collition_left:true/false
-        head_collition_right:true/false
-        head_collition_up:true/false
-        head_collition_down:true/false
-        green_apple_left:true/false
-        green_apple_right:true/false
-        green_apple_up:true/false
-        green_apple_down:true/false
-        red_apple_left:true/false
-        red_apple_right:true/false
-        red_apple_up:true/false
-        red_apple_down:true/false
-        """
-        if len(self.worn) == 0:
-            head_col = 1
-            head_raw = 1
-        else:
-            head_col = self.worn[0][0] + 1
-            head_raw = self.worn[0][1] + 1
-#        raw = self.map[head_raw]
-#        col = []
-#        for i in range(self.nr_cells[1] + 2):
-#            col.append(self.map[i][head_col])
-#        numbered_raw = [DICTIONARY_OBSERVATION[cell] for cell in raw]
-#        numbered_col = [DICTIONARY_OBSERVATION[cell] for cell in col]
-#        observation = numbered_raw + numbered_col
-        observation = [0 for _ in range(12)]
-        # Check for collisions with walls
-        if self.map[head_raw - 1][head_col] == "W" or \
-           self.map[head_raw - 1][head_col] in "S":
-            observation[0] = 1
-        if self.map[head_raw + 1][head_col] == "W" or \
-           self.map[head_raw + 1][head_col] in "S":
-            observation[1] = 1
-        if self.map[head_raw][head_col - 1] == "W" or \
-           self.map[head_raw][head_col - 1] in "S":
-            observation[2] = 1
-        if self.map[head_raw][head_col + 1] == "W" or \
-           self.map[head_raw][head_col + 1] in "S":
-            observation[3] = 1
-        # Check for coincidences in raw or col with green apples
-        for apple in self.green_apples:
-            if apple[0] < head_col - 1 and apple[1] == head_raw - 1:
-                observation[4] = 1
-            if apple[0] > head_col - 1 and apple[1] == head_raw - 1:
-                observation[5] = 1
-            if apple[0] == head_col - 1 and apple[1] > head_raw - 1:
-                observation[6] = 1
-            if apple[0] == head_col - 1 and apple[1] < head_raw - 1:
-                observation[7] = 1
-        # Check for coincidences in raw or col with red apples
-        for apple in self.red_apples:
-            if apple[0] < head_col - 1 and apple[1] == head_raw - 1:
-                observation[8] = 1
-            if apple[0] > head_col - 1 and apple[1] == head_raw - 1:
-                observation[9] = 1
-            if apple[0] == head_col - 1 and apple[1] > head_raw - 1:
-                observation[10] = 1
-            if apple[0] == head_col - 1 and apple[1] < head_raw - 1:
-                observation[11] = 1
-        return observation
+    def _learn_game(self, max_episodes=1000,learn_type="Q_LEARNING", FileNameModel="Model_Q.pt",gpu_number=0):
+        #env = EnvSnake(Nr_cells=[10, 10])
+        #agent = DQNAgent(state_shape=len(self.observation_space),
+        #                num_actions=self.action_space,
+        #                learning_type=learn_type,
+        #                filename=FileNameModel,
+        #                gpu_number=gpu_number)
+        # initialize counter of rewards for each action
+        rewards = [0 for _ in range(Reward.get_len() + 1)]
+        max_length = 3
+        first_time = time.time()
+        lengths = []
+        for i in range(max_episodes):
+            observation, info = self.reset()
+            episode_over = False
+            while not episode_over:
+                # agent policy that uses the observation and info
+                action, _ = self.agent.choose_action(observation)
+                next_observation, reward, terminated, truncated, info = self.step(action)
+                self.agent.store_experience(observation, action, reward.value, next_observation,
+                                       terminated or truncated)
+                self.agent.train_single_step(observation, action, reward.value, next_observation,
+                                       terminated or truncated)
+                observation = next_observation
+                if platform.system() == "Windows":
+                    os.system("cls")
+                else:
+                    os.system('clear')
+                #env.print_map_in_shell()
+                episode_over = terminated or truncated
+                max_length = max(self.get_length_worn(), max_length)
+                print(f"Episode {i + 1}/{max_episodes}", end="\t")
+                print(f"- Length {self.get_length_worn()}", end="\t")
+                print(f"- Max_length {max_length}", end="\t")
+                print(f"- Action: {Action(action).get_action_name()}")
+                print(f"Reward: {reward}", end="\t")
+                print(f"- Epsilon: {self.agent.epsilon:.4f}", end="\t")
+                print(f"- Terminated: {terminated}", end="\t")
+                print(f"- Truncated: {truncated}", end="\t")
+                #print(f"- Moves: {info['moves']}")
+                num_Chars = 50
+                percent = 100 * (i + 1) / max_episodes
+                filled = int(num_Chars * percent // 100)
+                bar = '█' * filled + '-' * (num_Chars - filled)
+                print(f'', end='\r')
+                print(f"Time elapsed: {(time.time() - first_time) / 3600:.2f} hours,",
+                        end="\t")
+                print(f"|{bar}| {percent:.2f}%", end="\t")
+                time_left = ((max_episodes - i - 1) * (time.time() - first_time) /
+                                (i + 1) /
+                                3600)
+                print(f"- time left: {time_left:.2f} hours")
+            self.agent.train_all()
+            lengths.append(self.get_length_worn())
+        episode_over = False
+        lengths = np.array(lengths)
+        plt.plot(lengths, label='Length of Snake')
+        plt.xlabel('Episode')
+        plt.ylabel('Length')
+        plt.title('Length of Snake Over Episodes')
+        plt.legend()
+        plt.show()
+
+        print("Training completed.")
+
+        self.agent.save_model(filename=FileNameModel)
+
 
     def _select_mode(self):
         """Display the mode selection menu."""
@@ -407,6 +419,7 @@ class Snake(pg.sprite.Sprite, MotorSnake):
         self.manual_button = pg.Rect(self.screen.get_width() // 2 - 100, 180, 200, 50)
         if self.show_autoplay:
             self.auto_button = pg.Rect(self.screen.get_width() // 2 - 100, 250, 200, 50)
+        self.learn_button = pg.Rect(self.screen.get_width() // 2 - 100, 320, 200, 50)
 
         while self.menu_active:
             # Title
@@ -417,14 +430,15 @@ class Snake(pg.sprite.Sprite, MotorSnake):
             pg.draw.rect(self.screen, (70, 130, 180), self.manual_button)  # Steel Blue
             if self.show_autoplay:
                 pg.draw.rect(self.screen, (34, 139, 34), self.auto_button)     # Forest Green
-
+            pg.draw.rect(self.screen, (255, 69, 0), self.learn_button)
             # Button text
             manual_text = self.info_font.render("Manual Mode", True, (255, 255, 255))
             auto_text = self.info_font.render("Auto Mode", True, (255, 255, 255))
             self.screen.blit(manual_text, (self.manual_button.x + 30, self.manual_button.y + 10))
             if self.show_autoplay:
                 self.screen.blit(auto_text, (self.auto_button.x + 40, self.auto_button.y + 10))
-
+            learn_text = self.info_font.render("Learn Mode", True, (255, 255, 255))
+            self.screen.blit(learn_text, (self.learn_button.x + 40, self.learn_button.y + 10))
             pg.display.flip()
             self._check_event_()
         pg.quit()
